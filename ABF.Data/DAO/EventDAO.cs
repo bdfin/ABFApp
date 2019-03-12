f79959cc3f165ae25eca7eea5f1d8f11d93bf374
bdfin <beau.d.findlay@gmail.com>
2019-03-12 20:30:44 +0000

    Add admin/events method, controller and view

---
+++ ABF.Data/DAO/EventDAO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABF.Data.ABFDbModels;
+
+namespace ABF.Data.DAO
+{
+    public class EventDAO
+    {
+        private ABFDbContext _context;
+
+        public EventDAO()
+        {
+            _context = new ABFDbContext();
+        }
+
+        public IList<Event> GetEvents()
+        {
+            IQueryable<Event> _events;
+
+            _events = from e
+                      in _context.Events
+                      select e;
+
+            return _events.ToList();
+        }
+    }
+}
---
+++ ABF.Service/Services/EventService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABF.Data.ABFDbModels;
+using ABF.Data.DAO;
+
+namespace ABF.Service.Services
+{
+    public class EventService
+    {
+        private EventDAO _eventDAO;
+
+        public EventService()
+        {
+            _eventDAO = new EventDAO();
+        }
+
+        public IList<Event> GetEvents()
+        {
+            return _eventDAO.GetEvents();
+        } 
+    }
+}
