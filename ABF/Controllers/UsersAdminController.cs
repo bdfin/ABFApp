f36850d97e0b0672d7e3bba11e867b8e817a0a0d
Daniel Carter <danielhattoncarter@yahoo.co.uk>
2019-03-18 14:40:25 +0000

    Fix error with edit user in User Management

--- ABF/Controllers/UsersAdminController.cs
+++ ABF/Controllers/UsersAdminController.cs
@@ -150,12 +150,12 @@ namespace ABF
                 PostCode = user.PostCode
             };
 
-            //editUserViewModel.RolesList = RoleManager.Roles.ToList().Select(x => new SelectListItem()
-            //{
-            //    Selected = userRoles.Contains(x.Name),
-            //    Text = x.Name,
-            //    Value = x.Name
-            //});
+            editUserViewModel.RolesList = RoleManager.Roles.ToList().Select(x => new SelectListItem()
+            {
+                Selected = userRoles.Contains(x.Name),
+                Text = x.Name,
+                Value = x.Name
+            });
 
             return View(editUserViewModel);
         }
