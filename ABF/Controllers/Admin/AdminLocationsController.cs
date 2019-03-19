2a9da55b7c3fb15cc3b31a00583010e356d070ff
bdfin <beau.d.findlay@gmail.com>
2019-03-19 12:39:08 +0000

    Add locations admin index page

---
+++ ABF/Controllers/Admin/AdminLocationsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ABF.Service.Services;
+
+namespace ABF.Controllers.Admin
+{
+    public class AdminLocationsController : Controller
+    {
+        private LocationService locationService;
+
+        public AdminLocationsController()
+        {
+            locationService = new LocationService();
+        }
+
+        [Route("Admin/Locations")]
+        public ActionResult Index()
+        {
+            return View(locationService.GetLocations());
+        }
+
+       
+    }
+}
