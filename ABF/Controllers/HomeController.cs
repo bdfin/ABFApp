9fd9987f827f935ad3103ca9ebbad3b2785c16c5
Dan <danielhattoncarter@yahoo.co.uk>
2019-03-23 19:00:17 +0000

    Add Home/visit.cshtml page with Google Maps API and JavaScript Search function

--- ABF/Controllers/HomeController.cs
+++ ABF/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace ABF.Controllers
             return View();
         }
 
+        public ActionResult Visit()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
